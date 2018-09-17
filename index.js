4833a00edc69c7fd29f397c12578d05b79e488cf
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-09-17 00:27:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// add solution here
+
+
+function theBeatlesPlay(arrMus, arrInst) {
+  var newArr = []
+  for (var i = 0; i < arrMus.length; i++){
+    
+    for (var j = 0; j < arrInst.length; j++) {
+      
+    newArr.push(arrMus[i] + " plays " + arrInst[j])
+    }
+    
+  console.log(newArr)
+
+  }
+}aZ
